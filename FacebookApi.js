ad6ce261527abd3863f44926b540559eef5aaf04
Alin Roman <alinroman@Alins-MacBook-Pro.local>
2015-10-06 12:04:57 +0300

    Initial commit
    Initial commit
    
    Added package.json file
    
    Renamed type property to protocol.
    
    Updated facebook stream to use the new stream-dev-tools.
    
    Added requestConfig callback.
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Made some changes :)
    
    Removed sensitive data.

---
+++ FacebookApi.js
@@ -0,0 +1,320 @@
+var Promise = require('bluebird');
+
+/**
+ * @param oauth
+ * @constructor
+ */
+var FacebookApi = function FacebookApi(oauth) {
+    this.oauth = oauth;
+};
+
+/**
+ * @param path {String}
+ * @param accessToken {String}
+ * @returns {Promise}
+ */
+FacebookApi.prototype.get = function (path, accessToken) {
+    var future = Promise.defer();
+
+    var url = 'https://graph.facebook.com/v2.4' + path;
+    this.oauth.get(url, accessToken, function (err, data) {
+        if (err) return future.reject(err);
+        try {
+            data = JSON.parse(data);
+        } catch (err) {
+            return future.reject(err);
+        }
+        future.resolve(data);
+    });
+
+    return future.promise.catch(FacebookApi.parseError);
+};
+
+FacebookApi.prototype._post = function(url, data, access_token, callback) {
+    if (this.oauth._useAuthorizationHeaderForGET) {
+        var headers= { Authorization: this.oauth.buildAuthHeader(access_token) };
+        access_token = null;
+    } else {
+        headers = { };
+    }
+    this.oauth._request('POST', url, headers, '', access_token, callback);
+};
+
+FacebookApi.prototype._delete = function(url, data, access_token, callback) {
+    if (this.oauth._useAuthorizationHeaderForGET) {
+        var headers= { Authorization: this.oauth.buildAuthHeader(access_token) };
+        access_token = null;
+    } else {
+        headers = { };
+    }
+    this.oauth._request('DELETE', url, headers, '', access_token, callback);
+};
+
+/**
+ * @param path {String}
+ * @param data {Object}
+ * @param accessToken {String}
+ * @returns {Promise}
+ */
+FacebookApi.prototype.post = function (path, data, accessToken) {
+    var future = Promise.defer();
+
+    var url = 'https://graph.facebook.com/v2.4' + path;
+    this._post(url, data, accessToken, function (err, data) {
+        if (err) return future.reject(err);
+        try {
+            data = JSON.parse(data);
+        } catch (err) {
+            return future.reject(err);
+        }
+        future.resolve(data);
+    });
+
+    return future.promise.catch(FacebookApi.parseError);
+};
+
+/**
+ * @param path {String}
+ * @param data {Object}
+ * @param accessToken {String}
+ * @returns {Promise}
+ */
+FacebookApi.prototype.delete = function (path, data, accessToken) {
+    var future = Promise.defer();
+
+    var url = 'https://graph.facebook.com/v2.4' + path;
+    this._delete(url, data, accessToken, function (err, data) {
+        if (err) return future.reject(err);
+        try {
+            data = JSON.parse(data);
+        } catch (err) {
+            return future.reject(err);
+        }
+        future.resolve(data);
+    });
+
+    return future.promise.catch(FacebookApi.parseError);
+};
+
+/**
+ * @param pageId {Number}
+ * @param accessToken {String}
+ * @returns {Promise}
+ */
+FacebookApi.prototype.getPageCounts = function (pageId, accessToken) {
+    return this.get(
+        '/' + pageId + '?fields=new_like_count,unread_message_count,unread_notif_count',
+        accessToken
+    ).then(function(data) {
+        return {
+            newLikes: data.new_like_count,
+            newMessages: data.unread_message_count,
+            newNotifications: data.unread_notif_count
+        };
+    });
+};
+
+/**
+ * @param accessToken {String}
+ * @returns {Promise}
+ */
+FacebookApi.prototype.getPages = function(accessToken) {
+    return this.get('/me?fields=accounts.limit(500){name}', accessToken)
+    .then(function(data) {
+        return (data && data.accounts && data.accounts.data) || [];
+    });
+};
+
+/**
+ * @param accessToken {String}
+ * @returns {Promise}
+ */
+FacebookApi.prototype.getUserId = function(accessToken) {
+    return this.get('/me?fields=id', accessToken)
+        .then(function(data) {
+            return data.id;
+        });
+};
+
+FacebookApi.prototype.subscribePageToRTUs = function(pageId, accessToken) {
+    var _this = this;
+    return this.getPageAccessToken(pageId, accessToken).then(function(pageAccessToken) {
+        return _this.post('/me/subscribed_apps', { id: pageId }, pageAccessToken);
+    }).then(function(data) {
+        return data.success;
+    });
+};
+
+FacebookApi.prototype.unsubscribePageFromRTUs = function(pageId, accessToken) {
+    var _this = this;
+    return this.getPageAccessToken(pageId, accessToken).then(function(pageAccessToken) {
+        return _this.delete('/me/subscribed_apps', { id: pageId }, pageAccessToken);
+    }).then(function(data) {
+        return data.success;
+    });
+};
+
+FacebookApi.prototype.getPageAccessToken = function(pageId, accessToken) {
+    return this.get('/' + pageId + '?fields=access_token', accessToken).then(function(data) {
+        return data.access_token;
+    });
+};
+
+FacebookApi.prototype.getLastPostCounts = function(account, accessToken) {
+    var counts = {
+        likes: 0,
+        comments: 0
+    };
+
+    return this.get(
+        '/' + account + '?fields=feed.limit(1){comments.limit(0).summary(1),likes.limit(0).summary(1)}',
+        accessToken
+    ).then(function(data) {
+        if (!data.feed || !data.feed.data || !data.feed.data[0]) {
+            return counts;
+        }
+        var post = data.feed.data[0];
+        if (post.likes && post.likes.summary) {
+            counts.likes = post.likes.summary.total_count || 0;
+        }
+        if (post.comments && post.comments.summary) {
+            counts.comments = post.comments.summary.total_count || 0;
+        }
+        return counts;
+    });
+};
+
+FacebookApi.prototype.getNextEvent = function(accessToken) {
+    return this.get('/me?fields=events.limit(100){name,rsvp_status,start_time,place{name}}', accessToken).then(function(data) {
+        if (!data || !data.events || !data.events.data || !data.events.data.length) {
+            return null;
+        }
+
+        var events = data.events.data.filter(function(event) {
+            if (['attending', 'unsure'].indexOf(event.rsvp_status) < 0) {
+                return false;
+            }
+
+            return Date.now() <= (new Date(event.start_time)).getTime();
+        }).sort(function(a, b) {
+            return (new Date(a.start_time)).getTime() - (new Date(b.start_time)).getTime();
+        });
+        if (!events[0]) return null;
+        var event = events[0];
+
+        return {
+            name: event.name,
+            location: event.place && event.place.name,
+            date: new Date(event.start_time)
+        };
+    });
+};
+
+FacebookApi.prototype.getPageDetails = function(pageId, accessToken) {
+    return this.get('/' + pageId + '?fields=name', accessToken).then(function(data) {
+        return {
+            name: data && data.name
+        };
+    });
+};
+
+FacebookApi.prototype.getWeeklyInsights = function(objectId, insights, accessToken) {
+    var aDay = 24 * 60 * 60;
+    var toUnix = 0.001;
+    var today = Math.floor(Date.now() / aDay * toUnix) * aDay;
+    var tomorrow = today + aDay;
+    var twoWeeksAgo = tomorrow - aDay  * 14;
+
+
+    var path = '/' + objectId + '/insights/' + insights + '/day?since=' + twoWeeksAgo + '&until=' + tomorrow;
+    return this.get(path, accessToken).then(function(data) {
+        var counts = {
+            thisWeek: 0,
+            lastWeek: 0
+        };
+        var values = data && data.data && data.data[0] && data.data[0].values;
+        if (!values) return counts;
+        values.sort(function(a, b) { return (new Date(a.end_time)).getTime() - (new Date(b.end_time)).getTime(); });
+        counts.lastWeek = values.slice(0, 7).reduce(function(sum, item) { return sum + Number(item.value); }, 0);
+        counts.thisWeek = values.slice(7).reduce(function(sum, item) { return sum + Number(item.value); }, 0);
+
+        return counts;
+    });
+};
+
+FacebookApi.prototype.getPageWeeklyReach = function(pageId, accessToken) {
+    return this.getWeeklyInsights(pageId, 'page_impressions_unique', accessToken);
+};
+
+FacebookApi.prototype.getPageWeeklyEngagement = function(pageId, accessToken) {
+    return this.getWeeklyInsights(pageId, 'page_engaged_users', accessToken);
+};
+
+FacebookApi.prototype.getPageWeeklyNewLikes = function(pageId, accessToken) {
+    return this.getWeeklyInsights(pageId, 'page_fan_adds', accessToken);
+};
+
+FacebookApi.prototype.getPageWeeklyLikes = function(pageId, accessToken) {
+    var aDay = 24 * 60 * 60;
+    var toUnix = 0.001;
+    var today = Math.floor(Date.now() / aDay * toUnix) * aDay;
+    var tomorrow = today + aDay;
+    var aWeekAgo = tomorrow - aDay  * 7;
+
+
+    var path = '/' + pageId + '/insights/page_fans?since=' + aWeekAgo + '&until=' + tomorrow;
+    return this.get(path, accessToken).then(function(data) {
+        var counts = {
+            thisWeek: 0,
+            lastWeek: 0
+        };
+        var values = data && data.data && data.data[0] && data.data[0].values;
+        if (!values || !Array.isArray(values)) return counts;
+        values.sort(function(a, b) { return (new Date(a.end_time)).getTime() - (new Date(b.end_time)).getTime(); });
+        counts.lastWeek = (values.shift() || {}).value || 0;
+        counts.thisWeek = (values.pop() || {}).value || 0;
+
+        return counts;
+    });
+};
+
+
+function FacebookApiError(rawError) {
+    this.raw = rawError;
+    this.message = rawError.message;
+    this.name = 'FacebookApiError';
+    Error.captureStackTrace(this, FacebookApiError);
+}
+FacebookApiError.prototype.getType = function() { return this.raw.type; };
+FacebookApiError.prototype.getRaw = function() { return this.raw; };
+FacebookApiError.prototype = Object.create(Error.prototype);
+FacebookApiError.prototype.constructor = FacebookApiError;
+
+function FacebookOAuthError(rawError) {
+    this.raw = rawError;
+    this.message = rawError.message;
+    this.name = 'FacebookOAuthError';
+    Error.captureStackTrace(this, FacebookOAuthError);
+}
+FacebookOAuthError.prototype = Object.create(FacebookApiError.prototype);
+FacebookOAuthError.prototype.constructor = FacebookOAuthError;
+
+FacebookApi.parseError = function(err) {
+    if (typeof err == 'string') {
+        err = JSON.parse(err);
+    }
+    if (!err.error) throw err;
+    if (!err.error.fbtrace_id) throw err;
+
+    err = new FacebookApiError(err.error);
+    if (err.getType() == 'OAuthException') {
+        err = new FacebookOAuthError(err.getRaw());
+    }
+
+    throw err;
+};
+
+FacebookApi.FacebookApiError = FacebookApiError;
+FacebookApi.FacebookOAuthError = FacebookOAuthError;
+
+module.exports = FacebookApi;
---
+++ run.js
@@ -0,0 +1,451 @@
+var Promise = require('bluebird'),
+    vectorWatch = require('stream-dev-tools'),
+    express = require('express'),
+    app = express(),
+    http = require('http'),
+    moment = require('moment'),
+    bodyParser = require('body-parser');
+
+
+var updateIntervalInMinutes = 15;
+
+var configJSON = {
+    streamUID: process.env.STREAM_UID,
+    token: process.env.VECTOR_TOKEN,
+
+    auth: {
+        protocol: 'OAuth',
+        version: '2.0',
+        accessTokenUrl: 'https://graph.facebook.com/oauth/access_token',
+        authorizeUrl: 'https://www.facebook.com/dialog/oauth?response_type=code&scope=manage_pages,read_insights,read_page_mailboxes,user_events,user_posts',
+        callbackUrl: 'http://vectorwatch-srv.cloudapp.net/facebook-stream/',
+        clientId: process.env.FB_APPID,
+        clientSecret: process.env.FB_SECRET
+    },
+
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'FacebookStream'
+    }
+};
+
+
+
+var vectorStream = vectorWatch.createStreamNode(configJSON);
+app.use('/facebook-stream/push', bodyParser.urlencoded({ extended: true }));
+app.use('/facebook-stream/push', bodyParser.json());
+app.get('/facebook-stream/push', function(req, res) {
+    if (req.param('hub.verify_token') == 'iacuceapa') {
+        if (req.param('hub.mode') == 'subscribe') {
+            res.status(200).send(req.param('hub.challenge'));
+            return;
+        }
+    }
+    res.status(400).send();
+});
+app.post('/facebook-stream/push', function(req, res) {
+    res.status(200).send('OK');
+
+    var entries = [];
+    req.body.entry.forEach(function(entry) {
+        var fields = req.body.object == 'user' ? entry.changed_fields : entry.changes;
+        fields.forEach(function(field) {
+            if (typeof field == 'string') {
+                field = { field: field };
+            }
+
+            entries.push({
+                id: entry.id,
+                time: entry.time,
+                field: field.field,
+                value: field.value
+            });
+        });
+    });
+    entries.sort(function(a, b) {
+        return a.time - b.time;
+    });
+
+    entries.forEach(function(entry) {
+        if (req.body.object == 'user') {
+            handleUserPush(entry);
+        } else if (req.body.object == 'page') {
+            handlePagePush(entry);
+        }
+    })
+});
+app.use('/facebook-stream', vectorStream.getMiddleware());
+
+var OAuth2 = require('oauth').OAuth2;
+var FacebookApi = require('./FacebookApi.js');
+var facebookApi = new FacebookApi(new OAuth2(
+    configJSON.auth.clientId,
+    configJSON.auth.clientSecret
+));
+
+var getAccessTokenForState = function(state) {
+    var future = Promise.defer();
+
+    vectorStream.getAuthTokensForState(state, function(err, authTokens) {
+        if (err) return future.reject(err);
+        future.resolve(authTokens && authTokens.access_token);
+    });
+
+    return future.promise;
+};
+
+var getStreamDataForState = function(state, accessToken) {
+    var accessTokenPromise;
+    if (accessToken) {
+        accessTokenPromise = Promise.resolve(accessToken);
+    } else {
+        accessTokenPromise = getAccessTokenForState(state);
+    }
+    var account = state.Account;
+    var from = state['Display From'];
+    var option = state['Display Option'];
+
+
+    return accessTokenPromise.then(function(accessToken) {
+        if (from == 'LAST_POST') {
+            return facebookApi.getLastPostCounts(account, accessToken).then(function(counts){
+                return 'L ' + counts.likes + ', C ' + counts.comments;
+            });
+        }
+
+        if (from == 'NEXT_EVENT') {
+            if (account != 'me') {
+                return 'N/A';
+            }
+
+            return facebookApi.getNextEvent(accessToken).then(function(event) {
+                if (!event) {
+                    return 'N/A';
+                }
+
+                var lines = [event.name];
+                if (event.location) lines.push(event.location);
+                var then = moment(event.date);
+                if (then.isSame(new Date(), 'day')) {
+                    lines.push(then.format('h:mm a'));
+                } else {
+                    lines.push(then.format('MMM Do YY'));
+                }
+                lines.push(then.fromNow(true) + ' left');
+
+                return lines.join('\n');
+            });
+        }
+
+        if (account == 'me') {
+            // from this point the account can't be 'me'
+            return 'N/A';
+        }
+
+        if (from == 'NOTIFICATIONS') {
+            return facebookApi.getPageCounts(account, accessToken).then(function(counts) {
+                return 'L ' + counts.newLikes + ', M ' + counts.newMessages + ', N ' + counts.newNotifications;
+            });
+        }
+
+        if (from == 'INSIGHTS') {
+            var formatWeeklyInsights = function(counts) {
+                var percent = 0;
+                if (counts.lastWeek != counts.thisWeek) {
+                    percent = (counts.thisWeek - counts.lastWeek) / Math.max(counts.thisWeek, counts.lastWeek);
+                }
+                var sign = percent == 0 ? '' : (percent < 0 ? '-' : '+');
+                percent = Math.abs(Math.round(percent * 1000) / 10);
+                return counts.thisWeek + ', ' + sign + percent + '%';
+            };
+
+            if (option == 'REACH') {
+                return facebookApi.getPageWeeklyReach(account, accessToken).then(formatWeeklyInsights);
+            } else if (option == 'ENGAGEMENT') {
+                return facebookApi.getPageWeeklyEngagement(account, accessToken).then(formatWeeklyInsights);
+            } else if (option == 'LIKES') {
+                return facebookApi.getPageWeeklyLikes(account, accessToken).then(formatWeeklyInsights);
+            } else if (option == 'NEW_LIKES') {
+                return facebookApi.getPageWeeklyNewLikes(account, accessToken).then(formatWeeklyInsights);
+            } else {
+                return 'N/A';
+            }
+        }
+
+        if (from == 'DETAILS') {
+            return facebookApi.getPageDetails(account, accessToken).then(function(details) {
+                return details.name;
+            });
+        }
+    });
+};
+
+var eligibleForPageRTUs = function(settings) {
+    var account = settings.Account;
+    var from = settings['Display From'];
+    return account != 'me' && (from == 'LAST_POST' || from == 'NOTIFICATIONS');
+};
+
+vectorStream.registerSettings = function(resolve, reject, settings, authTokens) {
+    if (!authTokens) {
+        return reject(new Error('Invalid auth supplied.'), 901);
+    }
+
+    if (eligibleForPageRTUs(settings)) {
+        var storageKey = 'pageid_' + settings.Account;
+        vectorStream.stateStorage.retrieve(storageKey, function(err, association) {
+            if (err) return console.error('Could not retrieve pageId-channelLabels associations', err.stack || err);
+
+            var callback = function(err) {
+                if (err) return console.error('Could not store pageId-channelLabels associations', err.stack || err);
+            };
+
+            if (!association) {
+                facebookApi.subscribePageToRTUs(settings.Account, authTokens.access_token).then(function() {
+                    association = [settings.channelLabel];
+                    vectorStream.stateStorage.store(storageKey, association, callback);
+                }).catch(function(err) {
+                    console.error('Could not subscribe page to RTUs', err.stack || err);
+                });
+            } else {
+                association.push(settings.channelLabel);
+                association = association.filter(function(value, index, self) {
+                    return self.indexOf(value) === index;
+                });
+                vectorStream.stateStorage.replace(storageKey, association, callback);
+            }
+        });
+    } else if (settings.Account == 'me') {
+        facebookApi.getUserId(authTokens.access_token).then(function(userId) {
+            var storageKey = 'userid_' + userId;
+            vectorStream.stateStorage.retrieve(storageKey, function(err, association) {
+                if (err) return console.error('Could not retrieve userId-channelLabels associations', err.stack || err);
+
+                var callback = function(err) {
+                    if (err) return console.error('Could not store userId-channelLabels associations', err.stack || err);
+                };
+
+                if (!association) {
+                    association = [settings.channelLabel];
+                    vectorStream.stateStorage.store(storageKey, association, callback);
+                } else {
+                    association.push(settings.channelLabel);
+                    vectorStream.stateStorage.replace(storageKey, association, callback);
+                }
+            });
+        }).catch(function(err) {
+            console.error('Could not get userId', err.stack || err);
+        });
+    }
+
+    // In parallel, we generate the stream data and send it back to the user
+    getStreamDataForState(settings, authTokens.access_token).then(function(data) {
+        resolve(data);
+    }).catch(FacebookApi.FacebookOAuthError, function(err) {
+        reject(err, 901);
+    }).catch(function(err) {
+        reject(err);
+    });
+};
+
+vectorStream.unregisterSettings = function(settings) {
+    if (eligibleForPageRTUs(settings)) {
+        var storageKey = 'pageid_' + settings.Account;
+        vectorStream.stateStorage.retrieve(storageKey, function (err, association) {
+            if (err) return console.error('Could not retrieve pageId-channelLabels associations', err.stack || err);
+            if (!association) return;
+            var index = association.indexOf(settings.Account);
+            if (index < 0) return;
+
+            var callback = function (err) {
+                if (err) return console.error('Coult not remove or update pageId-channelLabels associations', err.stack || err);
+            };
+
+            association.splice(index, 1);
+            if (association.length == 0) {
+                facebookApi.unsubscribePageFromRTUs(settings.Account);
+                vectorStream.stateStorage.remove(storageKey, callback);
+            } else {
+                vectorStream.stateStorage.replace(storageKey, association, callback);
+            }
+        });
+    }
+};
+
+vectorStream.requestConfig = function(resolve, reject, authTokens) {
+    facebookApi.getPages(authTokens.access_token).then(function(pages) {
+        var fbPages = pages.map(function (item) {
+            return {
+                name: item.name,
+                value: item.id
+            };
+        });
+        fbPages.unshift({
+            name: 'My Profile',
+            value: 'me'
+        });
+
+        resolve({
+            renderOptions: {
+                Account: {
+                    type: 'INPUT_LIST_STRICT',
+                    hint: 'Select your profile or one of your pages to display information from.',
+                    order: 0,
+                    dataType: 'STATIC'
+                },
+                'Display From': {
+                    type: 'GRID_LAYOUT',
+                    hint: 'Select the option you want to display information from.',
+                    order: 1,
+                    dataType: 'DYNAMIC',
+                    asYouType: false,
+                    minChars: 0
+                },
+                'Display Option': {
+                    type: 'GRID_LAYOUT',
+                    hint: 'Select the information you want to display.',
+                    order: 2,
+                    dataType: 'DYNAMIC',
+                    asYouType: false,
+                    minChars: 0
+                }
+            },
+            defaults: {
+                Account: { value: 'me' },
+                'Display From': { value: 'LAST_POST' },
+                'Display Option': { value: 'LIKES' }
+            },
+            settings: {
+                Account: fbPages,
+                'Display From': [
+                    { name: 'Latest Post', value: 'LAST_POST' },
+                    { name: 'Notifications', value: 'NOTIFICATIONS_BAR' },
+                    { name: 'Insights', value: 'INSIGHTS' }
+                ],
+                'Display Option': [
+                    { name: 'Total Likes', value: 'LIKES' },
+                    { name: 'New Likes', value: 'NEW_LIKES' },
+                    { name: 'Reach', value: 'REACH' },
+                    { name: 'Engagement', value: 'ENGAGEMENT' }
+                ]
+            }
+        });
+    }).catch(FacebookApi.FacebookOAuthError, function(err) {
+        reject(err, 901);
+    }).catch(function(err) {
+        reject(err);
+    });
+};
+
+vectorStream.requestOptions = function(resolve, reject, settingName, value, state/*, authTokens*/) {
+    var account = state.Account;
+    var from = state['Display From'];
+
+    if (settingName == 'Display From') {
+        if (account == 'me') {
+            resolve([
+                { name: 'Latest Post', value: 'LAST_POST' },
+                { name: 'Upcoming Event', value: 'NEXT_EVENT' }
+            ]);
+        } else {
+            resolve([
+                { name: 'Notifications', value: 'NOTIFICATIONS' },
+                { name: 'Insights', value: 'INSIGHTS' },
+                { name: 'Latest Post', value: 'LAST_POST' },
+                { name: 'Page Name', value: 'DETAILS' }
+            ]);
+        }
+    } else if (settingName == 'Display Option') {
+        if (from == 'LAST_POST') {
+            resolve([]);
+        } else if (from == 'NEXT_EVENT') {
+            resolve([]);
+        } else if (from == 'NOTIFICATIONS') {
+            resolve([]);
+        } else if (from == 'INSIGHTS') {
+            resolve([
+                { name: 'Total Likes', value: 'LIKES' },
+                { name: 'New Likes this Week', value: 'NEW_LIKES' },
+                { name: 'Reach', value: 'REACH' },
+                { name: 'Engagement', value: 'ENGAGEMENT' }
+            ]);
+        } else if (from == 'DETAILS') {
+            resolve([]);
+        } else {
+            reject(new Error('Invalid "Display From" value.'));
+        }
+    } else {
+        reject(new Error('Invalid settingName value.'));
+    }
+};
+
+
+setInterval(function() {
+    vectorStream.retrieveSettings(function(states) {
+        for (var channelLabel in states) {
+            (function(state) {
+                getStreamDataForState(state).then(function(value) {
+                    vectorStream.push(state, value, 0.1);
+                }).catch(function(err) {
+                    console.error('Could not get stream data for state.', err.stack || err);
+                });
+            })(states[channelLabel]);
+        }
+    }, function(err) {
+        console.error('Could not fetch settings from database.', err.stack || err);
+    });
+}, updateIntervalInMinutes * 60 * 1000);
+
+
+function handleUserPush(entry) {
+    if (entry.field != 'feed') {
+        return;
+    }
+
+    var storageKey = 'userid_' + entry.id;
+    vectorStream.stateStorage.retrieve(storageKey, function(err, association) {
+        if (err) return console.error('Could not retrieve userId-channelLabels associations', err.stack || err);
+        if (!association) return;
+
+        association.forEach(function(channelLabel) {
+            vectorStream.stateStorage.retrieve(channelLabel, function(err, state) {
+                if (err) return console.error('Could not retrieve state', err.stack || err);
+                getStreamDataForState(state).then(function(value) {
+                    vectorStream.push(state, value, 0.1);
+                }).catch(function(err) {
+                    console.error('Could not get stream data for state', err.stack || err);
+                });
+            });
+        });
+    });
+}
+
+function handlePagePush(entry) {
+    if (entry.field != 'conversations' || entry.field != 'feed') {
+        return;
+    }
+
+    var storageKey = 'pageid_' + entry.id;
+    vectorStream.stateStorage.retrieve(storageKey, function(err, association) {
+        if (err) return console.error('Could not retrieve pageId-channelLabels associations', err.stack || err);
+        if (!association) return;
+
+        association.forEach(function(channelLabel) {
+            vectorStream.stateStorage.retrieve(channelLabel, function(err, state) {
+                if (err) return console.error('Could not retrieve state', err.stack || err);
+                getStreamDataForState(state).then(function(value) {
+                    vectorStream.push(state, value, 0.1);
+                }).catch(function(err) {
+                    console.error('Could not get stream data for state', err.stack || err);
+                });
+            });
+        });
+    });
+}
+
+
+http.createServer(app).listen(8080, function() {
+    console.log('Non-secure server started.');
+});
